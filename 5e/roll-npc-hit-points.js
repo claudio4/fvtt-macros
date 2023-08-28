a544b12394d31c4dd818d3a180f6f985a4a65ec6
Claudio Yanes <me@claudio4.com>
2023-08-28 21:21:26 +0100

    Update Roll NPC hit points to remove warnings on V11

--- 5e/roll-npc-hit-points.js
+++ 5e/roll-npc-hit-points.js
@@ -1,18 +1,18 @@
 // Recommended icon icons/magic/life/heart-glowing-red.webp
 canvas.tokens.controlled.map(async (token) => {
   let actor = token.actor
-  if (actor.data.type !== "npc") {
+  if (actor.type !== "npc") {
     return
   }
 
-  let formula = token.actor?.data?.data?.attributes?.hp?.formula
+  let formula = token.actor?.system?.attributes?.hp?.formula
   if (!formula) {
     return
   }
 
   let roll = await (new Roll(formula).evaluate({ async: true }))
-  roll.toMessage({ flavor: 'Rolling ' + token.data.name + ' max HP' })
+  roll.toMessage({ flavor: 'Rolling ' + token.name + ' max HP' })
   let hp = roll.total
 
-  return actor.update({ "data.attributes.hp.value": hp, "data.attributes.hp.max": hp })
+  return actor.update({ "system.attributes.hp.value": hp, "system.attributes.hp.max": hp })
 })
